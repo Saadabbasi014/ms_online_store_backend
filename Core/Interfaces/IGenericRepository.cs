f89ab3e46da57359e3121753f96413e9c1a472d9
saad_abbasi <saad.abassi@orgonize.com>
2025-06-18 21:38:53 +0500

    generic repository added.

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);    
+        Task<bool> ExistsAsync(int id); 
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ Infrastructure/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,63 @@
+﻿using Core.Entites;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext context;
+
+        public GenericRepository(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            var entry = await context.Set<T>().AddAsync(entity);
+            return entry.Entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await context.Set<T>().FindAsync(id);
+            if (entity != null)
+            {
+                context.Set<T>().Remove(entity);
+            }
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await context.Set<T>().AnyAsync(x => x.Id == id);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync() > 0;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            context.Set<T>().Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+            await Task.CompletedTask;
+        }
+    }
+}
